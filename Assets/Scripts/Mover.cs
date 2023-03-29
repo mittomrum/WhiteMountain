425ac4e88fb203a10ea8cb52d58b793ecb41a77f
SchoolComputer <itla.main@gmail.com>
2023-03-29 12:35:30 +0200

    ClickToMove

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        if (target == null)
+        {
+            Debug.LogError("No Target added in the inspector!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Click To Move
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        #endregion
+        #region Debug
+        if (targetPosition !=  null)
+        {
+            Debug.DrawRay(transform.position, targetPosition - transform.position, Color.red, 0.1f);
+        }
+        #endregion
+    }
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            targetPosition = hit.point;
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+
+}
